1889f723b79d7b0c938423986362ef330b369dcd
leafeea <igorbudenyuk@gmail.com>
2024-11-29 08:15:02 +0200

    task elem design

--- src/components/task/task.jsx
+++ src/components/task/task.jsx
@@ -3,11 +3,17 @@ import classes from "./task.module.css";
 
 export const Task = (props) => {
     return (
-        <div className={classes.taskElem} {...props}>
-            <h2 className={classes.taskTitle}>{props.title}</h2>
-            <p className={classes.taskTag}>Tag: {props.tag}</p>
-            <p className={classes.taskStatus}>
-                Status: {!props.isComplete ? 'is being carried out' : 'completed'}
+        <div className={classes.task} {...props}>
+            <div className={classes.taskHeader}>
+                <h3 className={classes.taskTitle}>{props.title}</h3>
+                <span className={classes.taskTag}>{props.tag}</span>
+            </div>
+            <p
+                className={`${classes.taskStatus} ${
+                    props.isComplete ? classes.completed : classes.inProgress
+                }`}
+            >
+                {!props.isComplete ? 'is being carried out' : 'completed'}
             </p>
         </div>
     );
