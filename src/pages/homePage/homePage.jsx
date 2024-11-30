c3cae9cf1f37fd4152cabd9e1bd2eb3c8e335284
leafeea <igorbudenyuk@gmail.com>
2024-11-30 16:50:26 +0200

    filter added

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -9,12 +9,22 @@ export const HomePage = () => {
 
     const [tasks] = useContext(TasksContext);
     const [tags, setTags] = React.useState([]);
+    const [filteredTasks, setFilteredTasks] = React.useState(tasks);
 
     React.useEffect(() => {
         const uniqueTags = [...new Set(tasks.map(task => task.tag))];
         setTags(uniqueTags);
+        setFilteredTasks(tasks);
     }, [tasks]);
 
+    const filterByTag = (tag) => {
+        if (tag === "") {
+            setFilteredTasks(tasks);
+        } else {
+            const sortedTasks = tasks.filter(task => task.tag === tag);
+            setFilteredTasks(sortedTasks);
+        }
+    };
 
     return (
         <main>
@@ -22,10 +32,10 @@ export const HomePage = () => {
                 <h2 style={{textAlign:'center'}}>Welcome!</h2>
                 <ToDoForm></ToDoForm>
                 <div className={classes.toDoListWrapper}>
-                    <div className={`${classes.toDoListRow} ${classes.select}`}>
+                    <div className={`${classes.toDoListRow} ${classes.selectRow}`}>
                         <div className={classes.selectRow}>
                             <p>Show tag</p>
-                            <select>
+                            <select className={classes.selectTag} onChange={(e)=>{filterByTag(e.target.value)}}>
                                 <option value="">All tags</option>
                                 {tags.map((tag, index) => (
                                     <option key={index} value={tag}>{tag}</option>
@@ -34,7 +44,9 @@ export const HomePage = () => {
                         </div>
                     </div>
                     <div className={`${classes.toDoListRow} ${classes.list}`}>
-                        {tasks.map((task, index) => (<Task key={index} id={task.id} title={task.title} tag={task.tag} isComplete={task.isComplete}></Task>))}
+                        {filteredTasks.map((task, index) => (
+                            <Task key={index} id={task.id} title={task.title} tag={task.tag} isComplete={task.isComplete} />
+                        ))}
                     </div>
                 </div>
             </div>
